d1d2a0bda4b21c34b43712fbc2fc9e4dc25eb5b9
Yulia Novikova <ysn.novikova@gmail.com>
2025-07-18 22:01:07 +0200

    Add gifs

---
+++ gifs.py
@@ -0,0 +1,4 @@
+GIFS = [
+    "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnEzNXh6dGhwMGFxeHpmMHZ4bXdmbjVucWNid2hmMXRhMXNid2ZmeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/aLM8ZGtUHvXdqdo02T/giphy.gif",
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnEzNXh6dGhwMGFxeHpmMHZ4bXdmbjVucWNid2hmMXRhMXNid2ZmeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/XId87tyvWud70RjC6w/giphy.gif",
+]
--- soeder_bot.py
+++ soeder_bot.py
@@ -3,6 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import random
 import os
 from quotes import QUOTES
+from gifs import GIFS
 
 # load variables from the .env file
 from dotenv import load_dotenv
@@ -13,10 +14,20 @@ TOKEN = os.getenv('TELEGRAM_TOKEN')
 PORT = os.getenv('PORT', 4000)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Grüß Gott! 👋 Ich bin SöderDaily Bot.\nSchick mir /quote für eine stabile Weisheit.")
+    await update.message.reply_text(
+        "Grüß Gott! 👋 Ich bin SöderDaily Bot.\n\n"
+        "Schick mir:\n"
+        "• /quote für eine stabile Weisheit\n"
+        "• /gif für tägliches Södertainment"
+    )
+
 
 async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(random.choice(QUOTES) + "\n\n— Markus Söder")
+
+async def send_gif(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    gif_url = random.choice(GIFS)
+    await context.bot.send_animation(chat_id=update.effective_chat.id, animation=gif_url)
 
 if __name__ == '__main__':
     # Initialize the bot with token
@@ -25,6 +36,7 @@ if __name__ == '__main__':
     # Add handlers for commands
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("quote", quote))
+    app.add_handler(CommandHandler("gif", send_gif))
 
     print("Bot gestartet!")
     app.run_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN, webhook_url="https://soederdaily.onrender.com/" + TOKEN)
